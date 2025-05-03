d22930bc7eb0cbb53ac1b37d3923c04b38c9e118
RamspheldOnyangoOchieng <ramspheldonyango@gmail.com>
2025-05-04 00:43:41 +0300

    form routing  solved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import FormViewer from './FormViewer';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/form/:id" element={<FormViewer />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
