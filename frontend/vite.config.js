328b31e8e58aa4ce178ae41f6e06f05c12418c49
RamspheldOnyangoOchieng <ramspheldonyango@gmail.com>
2025-05-04 01:00:05 +0300

    frontend configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    historyApiFallback: true, // Ensure fallback to index.html in development
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
   },
   resolve: {
     alias: {
-      '@': '/src'
-    }
-  }
-})
+      '@': '/src',
+    },
+  },
+});
 // https://vitejs.dev/config/
